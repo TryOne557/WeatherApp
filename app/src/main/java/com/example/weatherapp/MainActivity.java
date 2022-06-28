654749b128563978341f00b9ec1947e0cdf75e93
TryOne557 <ilias.hatzikiriakos@edu.tbz.ch>
2022-06-28 09:52:27 +0200

    comit

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -3,12 +3,61 @@ package com.example.weatherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etCity, etCountry;
+    TextView tvResult;
+    public final String url = "http://api.openweathermap.org/data/2.5/weather";
+    private final String appid= "4c034d587340b228d7d28bc35ea8f1f7";
+    DecimalFormat df = new DecimalFormat("#.##");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etCity = findViewById(R.id.etCity);
+        etCountry = findViewById(R.id.etCountry);
+        tvResult= findViewById(R.id.tvResult);
+
+    }
+
+    public void getWeather(View view) {
+        String tempUrl = "";
+        String city = etCity.getText().toString().trim();
+        String country = etCountry.getText().toString().trim();
+        if (city.equals("")){
+            tvResult.setText("City filed can't be empty");
+        }else {
+            if(!country.equals("")){
+                tempUrl = url + "?q=" + city + "," + country + "&appid" + appid;
+            }else {
+                tempUrl = url + "?q=" + city + "&appid" + appid;
+            }
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, tempUrl, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Log.d("response", response);
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
     }
 }
+
